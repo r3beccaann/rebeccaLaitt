3c01dd0ba7804f3a6a1c7a3fc7e1207b05384abe
r3beccaann <rebeccalaitt@gmail.com>
2023-06-05 10:43:49 +0100

    Added weather php file and script

--- task/libs/js/script.js
+++ task/libs/js/script.js
@@ -1,12 +1,13 @@
 $(window).on('load', function () {
     if (
-        $('#preloader').length) {$('#preloader').delay(1000).fadeOut('slow',function () {
-            $(this).remove();
-        });
+        $('#preloader').length) {
+            $('#preloader').delay(1000).fadeOut('slow', function () {
+                $(this).remove();
+            });
     }
 });
 
-$('#earthquakeSubmit').click(function() {
+$('#earthquakeSubmit').click(function () {
 
     $.ajax({
         url: "libs/php/earthquake.php",
@@ -18,28 +19,66 @@ $('#earthquakeSubmit').click(function() {
             east: $('#eastInputE').val(),
             west: $('#westInputE').val()
         },
-        success: function(result) {
+        success: function (result) {
 
             console.log(JSON.stringify(result));
 
             if (result.status.name == "ok") {
 
-                $('#rowOneHeading').html('Date and Time:');
-                $('#rowTwoHeading').html('Depth:');
-                $('#rowThreeHeading').html('Magnitude:');
-                $('#rowFourHeading').html('Longitude:');
-                $('#rowFiveHeading').html('Latitude:');
-                $('#rowOneResult').html(result['data'][0]['datetime']);
-                $('#rowTwoResult').html(result['data'][0]['depth']);
-                $('#rowThreeResult').html(result['data'][0]['magnitude']);
-                $('#rowFourResult').html(result['data'][0]['lng']);
-                $('#rowFiveResult').html(result['data'][0]['lat']); 
+                $('#rowOneHeadingE').html('Date and Time:');
+                $('#rowTwoHeadingE').html('Depth:');
+                $('#rowThreeHeadingE').html('Magnitude:');
+                $('#rowFourHeadingE').html('Longitude:');
+                $('#rowFiveHeadingE').html('Latitude:');
+                $('#rowOneResultE').html(result['data'][0]['datetime']);
+                $('#rowTwoResultE').html(result['data'][0]['depth']);
+                $('#rowThreeResultE').html(result['data'][0]['magnitude']);
+                $('#rowFourResultE').html(result['data'][0]['lng']);
+                $('#rowFiveResultE').html(result['data'][0]['lat']);
             }
-        
+
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             console.log(jqXHR);
         }
-    }); 
+    });
+
+});
+
+$('#weatherSubmit').click(function () {
+
+    $.ajax({
+        url: "libs/php/weather.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#northInputW').val(),
+            south: $('#southInputW').val(),
+            east: $('#eastInputW').val(),
+            west: $('#westInputW').val()
+        },
+        success: function (result) {
+
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+
+                $('#rowOneHeadingW').html('Date and Time:');
+                $('#rowTwoHeadingW').html('Temperature:');
+                $('#rowThreeHeadingW').html('Clouds:');
+                $('#rowFourHeadingW').html('Wind Speed:');
+                $('#rowFiveHeadingW').html('Wind Direction:');
+                $('#rowOneResultW').html(result['data'][0]['datetime']);
+                $('#rowTwoResultW').html(result['data'][0]['temperature']);
+                $('#rowThreeResultW').html(result['data'][0]['clouds']);
+                $('#rowFourResultW').html(result['data'][0]['windSpeed']);
+                $('#rowFiveResultW').html(result['data'][0]['windDirection']);
+            }
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+        }
+    });
 
 });
