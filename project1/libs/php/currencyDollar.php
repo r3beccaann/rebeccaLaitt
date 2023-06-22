50e2e42d07b689c958d4c92cce1d3ea5bd7ddbac
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-22 16:13:48 +0100

    Update currencyDollar.php

--- project1/libs/php/currencyDollar.php
+++ project1/libs/php/currencyDollar.php
@@ -2,7 +2,6 @@
 
 $executionStartTime = microtime(true);
 
-// Initiate curl session
 $ch = curl_init();
 
 $url = "https://api.exchangerate.host/latest?base=USD";
@@ -12,14 +11,14 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-// curl execute and variable store
+
 $resp = curl_exec($ch);
 
 if ($e = curl_error($ch)) {
     echo $e;
 } else {
     $decoded = json_decode($resp, true);
-    //print_r($decoded);
+
 }
 
 curl_close($ch);
