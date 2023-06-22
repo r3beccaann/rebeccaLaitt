7572419e732f53da49cb0315bbd8c98c6a21ebf6
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-22 16:11:32 +0100

    Update countryCode.php

--- project1/libs/php/countryCode.php
+++ project1/libs/php/countryCode.php
@@ -1,32 +1,10 @@
-<?php 
+<?php
 
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
+$executionStartTime = microtime(true);
 
-    $executionStartTime = microtime(true);
+$countryData = json_decode(file_get_contents('../json/getCountryBorders.geo.json'));
 
-    $countryData = json_decode(file_get_contents("../json/getCountryBorders.json"));
-    // $countryInfo;
-
-    // foreach($countryData['features'] as $feature) {
-    //     $name = $feature["properties"]["name"];
-	// 	$code = $feature["properties"]["iso_a2"];
-    //     $countryInfo[$name] = $code;
-    // }
-
-    // // $decode = json_decode($countryData,true);	
-
-	// $output['status']['code'] = "200";
-	// $output['status']['name'] = "ok";
-	// $output['status']['description'] = "success";
-	// $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	// $output['data'] = $countryInfo;
-	
-	// header('Content-Type: application/json; charset=UTF-8');
-
-	// echo json_encode($countryInfo);
-
-    $features = $countryData->features;
+$features = $countryData->features;
 
 $countries = array();
 for ($i = 0; $i < sizeof($features); $i++) {
@@ -40,4 +18,7 @@ usort($countries, function ($a, $b) {
     return strcasecmp($a[0], $b[0]);
 });
 echo(json_encode($countries));
+
+
+
 ?>
