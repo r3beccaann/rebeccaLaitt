dfa6a690cf4f0ab887e82be493afcc66aa628f0b
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-23 16:00:28 +0100

    Update countryCode.php

--- project1/libs/php/countryCode.php
+++ project1/libs/php/countryCode.php
@@ -1,11 +1,15 @@
 <?php
 
+// gets current time
 $executionStartTime = microtime(true);
 
+// loads file into countryData variable
 $countryData = json_decode(file_get_contents('../json/getCountryBorders.geo.json'));
 
+// assigns features within countryData to features variable
 $features = $countryData->features;
 
+// iterates through features array. for each country, we get the country name and ISO-A2 code which are stored in an array and pushed into the countries array
 $countries = array();
 for ($i = 0; $i < sizeof($features); $i++) {
     $feature = $features[$i];
@@ -14,8 +18,8 @@ for ($i = 0; $i < sizeof($features); $i++) {
     $array = [$countryName, $countryIsoA2];
     array_push($countries, $array);
 }
-usort($countries, function ($a, $b) {
-    return strcasecmp($a[0], $b[0]);
+usort($countries, function ($a, $b) { // sorts the countries by country name
+    return strcasecmp($a[0], $b[0]); // compares the country names in a case insensitive way
 });
 echo(json_encode($countries));
 
