750a8b63e7d7050a2868c213c5998adc0ffc9810
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-23 15:56:40 +0100

    Update countryCities.php

--- project1/libs/php/countryCities.php
+++ project1/libs/php/countryCities.php
@@ -1,33 +1,32 @@
 <?php
 
+// gets current time
 $executionStartTime = microtime(true) / 1000;
 
+// creating curl handle
 $ch = curl_init();
-
-/* $north = $_GET['northBounds'];
-$south = $_GET['southBounds'];
-$east = $_GET['eastBounds'];
-$west = $_GET['westBounds'];  */
 
 //$url='http://api.geonames.org/citiesJSON?north=58.63500010846633&south=49.95999990498108&east=1.681530795914739&west=-7.572167934591079&lang=en&username=loud_task';
 
+// getting bounds from request variable
 $url = 'http://api.geonames.org/citiesJSON?north=' . $_REQUEST['northBounds'] . '&south=' . $_REQUEST['southBounds'] . '&east=' . $_REQUEST['eastBounds'] . '&west=' . $_REQUEST['westBounds'] . '&lang=en&username=rebeccalaitt';
 
 
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // ensures the response from the API is a string
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // disables SSL certificate verification
 
+// makes the request to the API
+$resp = curl_exec($ch); // response is stored in this variable
 
-$resp = curl_exec($ch);
-
+// error message if error
 if ($e = curl_error($ch)) {
     echo $e;
 } else {
-    $decoded = json_decode($resp, true);
+    $decoded = json_decode($resp, true); // decodes the response from the API as a a JSON object
 }
 
-curl_close($ch);
+curl_close($ch);  // closes the curl handle
 
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
