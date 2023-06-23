52c5749a3f6cfc58a9c6a6e5e1e8d6bad52f27de
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-23 15:59:47 +0100

    Update countryBorder.php

--- project1/libs/php/countryBorder.php
+++ project1/libs/php/countryBorder.php
@@ -1,24 +1,27 @@
-<?php 
+<?php
 
-    $executionStartTime = microtime(true);
+// gets current time
+$executionStartTime = microtime(true);
 
-    $countryBorder = json_decode(file_get_contents("../json/getCountryBorders.geo.json"));
+// loads file into countryBorder variable
+$countryBorder = json_decode(file_get_contents("../json/getCountryBorders.geo.json"));
 
+// assigns features within countryBorder to features variable
+$features = $countryBorder->features;
 
-    $features = $countryBorder->features;
+// getting country code
+$countryCode = $_GET['countryCode'];
 
-    $countryCode = $_GET['countryCode'];
+// $countryCode = 'GB';
 
-    // $countryCode = 'GB';
-   
-    $output = "";
+// iterating through the features array, checking the country code of the border is equal to the country code in the GET request
+$output = "";
 for ($i = 0; $i < sizeof($features); $i++) {
     $feature = $features[$i];
     if ($feature->properties->iso_a2 == $countryCode) {
         $output = $feature->geometry;
     }
 }
-echo(json_encode($output));
+echo (json_encode($output));
 
 ?>
-
