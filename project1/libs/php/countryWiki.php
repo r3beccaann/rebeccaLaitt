ec2e0c9290a9409771333e9343b7575e798a9e90
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-27 14:19:39 +0100

    Update countryWiki.php

--- project1/libs/php/countryWiki.php
+++ project1/libs/php/countryWiki.php
@@ -1,26 +1,30 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
 
+// error reporting
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
+
+// gets current time
 $executionStartTime = microtime(true);
 
-
+// getting country name from request variable
 $url = 'http://api.geonames.org/wikipediaSearchJSON?q=' .  $_REQUEST['countryName'] . '&maxRows=10&username=rebeccalaitt';
 
+// below used to test path
 // $url = 'http://api.geonames.org/wikipediaSearchJSON?q=united_kingdom&maxRows=10&username=rebeccalaitt';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ch = curl_init(); // creating curl handle
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // disables SSL certificate verification
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // ensures the response from the API is a string
 curl_setopt($ch, CURLOPT_URL,$url);
 
 
+// makes the request to the API
+$result=curl_exec($ch); // response is stored in this variable
 
-$result=curl_exec($ch);
+curl_close($ch);  // closes the curl handle
 
-curl_close($ch);
-
-$decode = json_decode($result,true);	
+$decode = json_decode($result,true);  // decodes the response from the API as a a JSON object
 
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
