1e65a82f8983cec69ee26df6120450789348ec9b
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-27 14:18:55 +0100

    Update countryLatLng.php

--- project1/libs/php/countryLatLng.php
+++ project1/libs/php/countryLatLng.php
@@ -1,13 +1,16 @@
 <?php
 
-//$url = "http://api.geonames.org/countryCodeJSON?lat=51.5072&lng=0.1276&username=loud_task";
+// url for testing
+//$url = "http://api.geonames.org/countryCodeJSON?lat=51.5072&lng=0.1276&username=rebeccalaitt";
 
+// lat and lng are received and stores into variables
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 
+// getting lat and lng from request variable
 $data = file_get_contents("http://api.geonames.org/countryCodeJSON?lat=$lat&lng=$lng&username=rebeccalaitt");
 
-$json = json_decode($data, true);
+$json = json_decode($data, true);  // decodes the response from the API as a a JSON object
 echo json_encode($json);
 
 
