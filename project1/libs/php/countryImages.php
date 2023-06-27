80ffb610722758032cc3dac6cef0851a59303d06
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-27 14:18:27 +0100

    Update countryImages.php

--- project1/libs/php/countryImages.php
+++ project1/libs/php/countryImages.php
@@ -1,32 +1,33 @@
 <?php
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+// error reporting
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
 
-	$executionStartTime = microtime(true);
+// gets current time
+$executionStartTime = microtime(true);
 
-    // $url='https://api.unsplash.com/search/photos/?page=1&query=Italy&client_id=DJ-VQYgmz14UuQwht7OBfkaRPpbCMYmFgWV6NYiikrE';
-	$url="https://api.unsplash.com/search/photos/?page=1&query=" . $_REQUEST['country'] . "&client_id=uEs0Cw8VIAFBtgCCjJ030MUJ8LouIQz5pv5HZS1TjEU";
+// getting country code from request variable
+$url = "https://api.unsplash.com/search/photos/?page=1&query=" . $_REQUEST['country'] . "&client_id=uEs0Cw8VIAFBtgCCjJ030MUJ8LouIQz5pv5HZS1TjEU";
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // disables SSL certificate verification
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // ensures the response from the API is a string
+curl_setopt($ch, CURLOPT_URL, $url);
 
-	$result=curl_exec($ch);
+// makes the request to the API
+$result = curl_exec($ch); // response is stored in this variable
 
-	curl_close($ch);
+curl_close($ch); // closes the curl handle
 
-	$decode = json_decode($result,true);	
+$decode = json_decode($result, true); // decodes the response from the API as a a JSON object
 
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['results'];
-	
-	header('Content-Type: application/json; charset=UTF-8');
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode['results'];
 
-	echo json_encode($output); 
+header('Content-Type: application/json; charset=UTF-8');
 
-?>
+echo json_encode($output);
