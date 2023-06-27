c6572ee5dbdbf16b9c64ecd3c8f4c6dc53bda82a
r3beccaann <106391103+r3beccaann@users.noreply.github.com>
2023-06-27 14:19:23 +0100

    Update countryWeather.php

--- project1/libs/php/countryWeather.php
+++ project1/libs/php/countryWeather.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
 
-    $executionStartTime = microtime(true);
+	$executionStartTime = microtime(true);
 
-    $url='http://api.geonames.org/weatherJSON?&north=' . $_REQUEST['northBounds'] . '&south=' . $_REQUEST['southBounds'] . '&east=' . $_REQUEST['eastBounds'] . '&west=' . $_REQUEST['westBounds'] . '&username=rebeccalaitt&style=full';
-
-    $ch = curl_init();
+//    $url = "https://api.openweathermap.org/data/2.5/weather?q=London,GB&appid=6a48e02a6f3898d055c1ea2e09537dd2";
+    $url='http://api.openweathermap.org/data/2.5/forecast?q=' . $_REQUEST['capital'] . ',' . $_REQUEST['countryCode'] . '&appid=6a48e02a6f3898d055c1ea2e09537dd2&units=metric';
+	
+	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_URL,$url);
@@ -16,15 +17,16 @@
 
 	curl_close($ch);
 
-    $decode = json_decode($result,true);	
+	$decode = json_decode($result,true);	
 
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['weatherObservations'];
+	$output['data'] = $decode;
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
-	echo json_encode($output);
+	echo json_encode($output); 
+
 ?>
